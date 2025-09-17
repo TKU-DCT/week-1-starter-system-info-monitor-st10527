bbcadd5692e9f0b3abe99093d0de64dbce8261f2
shengzhi <167320@o365.tku.edu.tw>
2025-09-17 00:50:46 +0000

    Finished Week 1 assignment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     print(f"CPU Usage: {psutil.cpu_percent(interval=1)}%")
     print(f"Memory Usage: {psutil.virtual_memory().percent}%")
     print(f"Disk Usage: {psutil.disk_usage('/').percent}%")
+    print("Done!")
 
 if __name__ == "__main__":
     main()
